949c98fc99bda4d3bee84bfc1c54b7a56f88454a
jkkim <jack68@naver.com>
2022-02-09 11:38:32 +0900

    listview add

--- lib/main.dart
+++ lib/main.dart
@@ -35,11 +35,42 @@ class RandomWords extends StatefulWidget{
 }
 
 class _RandomWordState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    // TODO: implement build
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions(){
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (context, i) {
+          if(i.isOdd){
+            return const Divider();
+          }
+          final index = i ~/2;
+          if(index >= _suggestions.length){
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style:_biggerFont,
+      )
+    );
+
   }
 }
 
